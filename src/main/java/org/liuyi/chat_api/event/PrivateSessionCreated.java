f004510186b4df9acf0479255510379e8179fc6f
liuyi <3319233753@qq.com>
2026-04-13 12:05:28 +0800

    新增PrivateSessionCreated事件

---
+++ src/main/java/org/liuyi/chat_api/event/PrivateSessionCreated.java
@@ -0,0 +1,26 @@
+package org.liuyi.chat_api.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.liuyi.common.domain.event.Event;
+
+import java.time.Instant;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PrivateSessionCreated extends Event {
+    public static String TOPIC = "private-session-created";
+    Instant createTime;
+    String sessionId;
+    private String userId1;
+    private String userId2;
+
+    @Override
+    public String getTopic() {
+        return TOPIC;
+    }
+}
